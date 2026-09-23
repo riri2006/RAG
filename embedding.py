203f6f460eb44425f38b64c8a686d81af8969d2d
riri2006 <riddhiverma001@gmail.com>
2026-09-23 23:53:40 +0530

    embedding

--- embedding.py
+++ embedding.py
@@ -2,6 +2,6 @@ from langchain_ollama import OllamaEmbeddings
 
 embeddings = OllamaEmbeddings(model="nomic-embed-text")
 
-vector = embeddings.embed_query("Hello, how are you?")
+vector = embeddings.embed_query("Hello I am Pizza.. Are you burger??")
 
 print(vector)
