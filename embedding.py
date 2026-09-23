bdb1d4125d9cb38649fdff09224644f42657562f
riri2006 <riddhiverma001@gmail.com>
2026-09-23 23:57:26 +0530

    embedding

--- embedding.py
+++ embedding.py
@@ -4,4 +4,6 @@ embeddings = OllamaEmbeddings(model="nomic-embed-text")
 
 vector = embeddings.embed_query("Hello I am Pizza.. Are you burger??")
 
-print(vector)
+print("type: ",type(vector))
+print("Length: ",len(vector))
+print("embedding: ", vector)
