385667805aa64c58adcf9b10477b212dd195aa23
riri2006 <riddhiverma001@gmail.com>
2026-09-24 12:11:53 +0530

    Vector db practice

--- BasicLearing/vectorDBpractice.py
+++ BasicLearing/vectorDBpractice.py
@@ -2,6 +2,16 @@ from langchain_core.documents import Document
 from langchain_ollama import OllamaEmbeddings
 
 doc1=Document(
-    page_content="Apple is a sweet and crunchy fruit. It is commonly red, green, or yellow and is a good source of fiber.",
+    page_content="This document consists of red apple",
     metadata={"fruit":"Apple"}
+)
+
+doc2=Document(
+    page_content="This documents consists of yellow mango",
+    metadata={"fruit":"Mango"}
+)
+
+doc3=Document(
+    page_content="This document consists of pink lychee ",
+    metadata={"fruit":"Lychee"}
 )
