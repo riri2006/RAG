0931916ba5470729c0f7a7facbb5aabf48416e38
riri2006 <riddhiverma001@gmail.com>
2026-09-24 22:41:51 +0530

    loader

--- BasicLearing/WebBaseLoader.py
+++
@@ -1,6 +0,0 @@
-from langchain_community.document_loaders import WebBaseLoader
-
-loader = WebBaseLoader(web_path="https://www.sap.com/india/products/financial-management.html?url_id=banner-in-homepage-row4-pos1-financial-management-260512")
-
-data=loader.load()
-print(data[0].page_content)
