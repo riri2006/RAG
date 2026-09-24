1680fd5931b1a63ef6b050ed8f94f8137516757d
riri2006 <riddhiverma001@gmail.com>
2026-09-24 12:22:18 +0530

    vdb with pretty print

--- BasicLearing/vectorDBpractice.py
+++ BasicLearing/vectorDBpractice.py
@@ -1,5 +1,7 @@
 from langchain_core.documents import Document
 from langchain_ollama import OllamaEmbeddings
+from langchain_chroma import Chroma
+from pprint import pprint
 
 doc1=Document(
     page_content="This document consists of red apple",
@@ -15,3 +17,17 @@ doc3=Document(
     page_content="This document consists of pink lychee ",
     metadata={"fruit":"Lychee"}
 )
+
+docs=[doc1,doc2,doc3]
+
+emebedding = OllamaEmbeddings(model="nomic-embed-text")
+
+vdb=Chroma(
+    embedding_function=emebedding,
+    persist_directory="chroma_db",
+    collection_name="Sampling"
+)
+
+vdb.add_documents(docs)
+result=vdb.get(include=['embeddings','documents','metadatas'])
+pprint(result)
